4f0ab0f638d6b9257a536c8e342c1a7a495ff897
LEOR3 <99679367+LEOR3@users.noreply.github.com>
2022-04-27 14:25:55 -0500

    Add files via upload

---
+++ src/views/Menu.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="home">
+    <header class="fixed">
+      <img alt="Pasteleria-logo" src="../assets/DTentaciones.png" class="logo">
+      <div class="top"></div>
+    </header>
+    <main class="info">
+      <h1 class="title">Pasteleria <br> <span class="subtitle"> Dulces Tentaciones </span>  </h1>
+
+      <p class="legend">La manera mas dulce de disfrutar la vida, es endulzandola con un pastel</p>
+
+      <br>
+      <h2 class="menu-title">Menu de opciones</h2>
+      <br>
+
+      <div class="menu">
+        <DecorationCake/>
+        <TasteCake/>
+        <Size/>
+      </div>
+
+      <br>
+      <h2 class="build-title">Arma tu propio Pastel !!!</h2>
+      <br>
+
+      <form @change="$store.dispatch('changeSummary')">
+        <div class="container-main">
+          <div class="requirements">
+            <TasteCombination/>
+            <DecorationCombination/>
+          </div>
+          <img src="https://www.bakeryandsnacks.com/var/wrbm_gb_food_pharma/storage/images/publications/food-beverage-nutrition/bakeryandsnacks.com/article/2018/10/09/have-your-cake-and-eat-it-brits-top-20-favorite-cakes/8701145-2-eng-GB/Have-your-cake-and-eat-it-Brits-Top-20-favorite-cakes.jpg" alt="cake-shop" class="cake-shop">
+        </div>
+        <DecorationTable/>
+        <SizeCake/>
+        <ClientContact/>
+        <Buttons/>
+      </form>
+    </main>
+    <footer class="menu-page">
+      <Contact/> 
+    </footer>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import Contact from "../components/Contact.vue";
+import TasteCake from "../components/TasteCake.vue";
+import DecorationCake from "../components/DecorationCake.vue";
+import SizeCake from "../components/SizeCake.vue";
+import ClientContact from "../components/ClientContact.vue";
+import Buttons from "../components/Buttons.vue";
+import DecorationCombination from "../components/DecorationCombination.vue";
+import TasteCombination from "../components/TasteCombination.vue";
+import Size from "../components/Size.vue";
+import DecorationTable from "../components/DecorationTable.vue";
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    Contact,
+    TasteCake,
+    DecorationCake,
+    SizeCake,
+    ClientContact,
+    Buttons,
+    DecorationCombination,
+    TasteCombination,
+    Size,
+    DecorationTable,
+  }
+}
+</script>
+
---
+++ src/views/Pastelero.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about">
+    <br><br><br>
+    <img alt="Pasteleria-logo" src="../assets/DTentaciones.png" class="logo">
+    <div class="top"></div>
+    <main class="info2">
+        <br><br>
+        <h1>Pastelero</h1>
+
+        <h2>Informacion General de la Pasteleria</h2>
+
+        <QtyTaste/>
+        <QtyDecoration/>
+        <Orders/>
+    
+    </main>
+    <footer class="pastelero-page">
+        <Contact/>
+    </footer>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import Contact from "../components/Contact.vue";
+import QtyTaste from "../components/QtyTaste.vue";
+import QtyDecoration from "../components/QtyDecoration.vue";
+import Orders from "../components/Orders.vue";
+
+export default {
+  name: 'HomeView',
+  components: {
+    Contact,
+    QtyTaste,
+    QtyDecoration,
+    Orders,
+  }
+}
+</script>
+
+<style scoped>
+  .about{
+    text-align: center;
+  }
+</style>
